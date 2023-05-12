453c8aedde4bb303c8703adb2a891200f7eb4b35
Francisco Metzger <francisco.metzger@gmail.com>
2023-05-12 18:17:56 +0200

    feat(report): implement basic jira service to access issue information

--- src/Reportify/Report/Jira/JiraService.cs
+++ src/Reportify/Report/Jira/JiraService.cs
@@ -1,9 +1,34 @@
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Options;
+using Reportify.Configuration;
+
 namespace Reportify.Report.Jira;
 
 internal class JiraService : IJiraService
 {
-  public Task<int?> GetErpPositionByIssueKey(string issueKey, CancellationToken cancellationToken = default)
+  private readonly HttpClient _httpClient;
+
+  public JiraService(HttpClient httpClient, IOptions<ReportifyOptions> options)
   {
-    return Task.FromResult<int?>(999999);
+    _httpClient = httpClient;
+    _httpClient.BaseAddress = new Uri($"{options.Value.JiraUrl}/rest/api/2/issue/");
+  }
+
+  public async Task<int?> GetErpPositionByIssueKey(string issueKey, CancellationToken cancellationToken = default)
+  {
+    var test = await _httpClient.GetAsync(issueKey, cancellationToken);
+    if (!test.IsSuccessStatusCode)
+    {
+      return null;
+    }
+
+    var issue = await test.Content.ReadFromJsonAsync<Issue>(cancellationToken: cancellationToken);
+    return issue?.Fields.ErpPosition;
   }
 }
+
+file sealed record Issue([property: JsonPropertyName("fields")] Fields Fields);
+
+file sealed record Fields([property: JsonPropertyName("customfield_10701")]
+  int? ErpPosition);
