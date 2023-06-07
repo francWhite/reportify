e8f1d6fb3a98f514562b011da21330ca5c4294b0
Francisco Metzger <francisco.metzger@gmail.com>
2023-06-07 18:04:44 +0200

    fix(cli): parse date with current culture instead of invariant

---
+++ src/Reportify/Commands/DateOnlyCurrentCultureConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace Reportify.Commands;
+
+//workaround because Spectre.Console.Cli does always use InvariantCulture
+internal class DateOnlyCurrentCultureConverter : DateOnlyConverter
+{
+  public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+  {
+    return base.ConvertFrom(context, CultureInfo.CurrentCulture, value);
+  }
+}
