dc49dee559e36b40c1703f8d76161b3f878aa2d0
Francisco Metzger <francisco.metzger@gmail.com>
2023-06-30 13:00:18 +0200

    fix(configuration): print actual location of configuration file

---
+++ src/Reportify/Configuration/ConfigurationFileInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Reportify.Configuration;
+
+internal static class ConfigurationFileInfo
+{
+  public const string FileName = ".reportify";
+  public static string FolderPath =>  Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+  public static string FilePath =>  Path.Combine(FolderPath, FileName);
+}
--- src/Reportify/Configuration/IConfigurationValidator.cs
+++ src/Reportify/Configuration/IConfigurationValidator.cs
@@ -1,4 +1,4 @@
-﻿namespace Reportify.Configuration.Validation;
+﻿namespace Reportify.Configuration;
 
 internal interface IConfigurationValidator
 {
