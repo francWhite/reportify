3eae945e177657c9eb5cfd191008d89408bcde11
francWhite <francisco.metzger@gmail.com>
2023-07-13 13:26:36 +0200

    refactor: removed obsolete todo

--- src/Reportify/Configuration/ManicTimeOptionsExtensions.cs
+++ src/Reportify/Configuration/ManicTimeOptionsExtensions.cs
@@ -6,7 +6,6 @@ internal static class ManicTimeOptionsExtensions
 {
   public static string BuildConnectionString(this ManicTimeOptions options)
   {
-    //TODO throw if file doesn't exists
     var builder = new SqliteConnectionStringBuilder
     {
       DataSource = options.DatabasePath,
