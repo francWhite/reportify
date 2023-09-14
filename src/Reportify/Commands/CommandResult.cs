411f1b21eea747424308b7ca2f84995bd7a105f1
Francisco Metzger <francisco.metzger@gmail.com>
2023-09-14 17:47:49 +0200

    refactor(validation): only execute configuration validation if configuration is required for the requested command

---
+++ src/Reportify/Commands/CommandResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Reportify.Commands;
+
+internal enum CommandResult
+{
+  Success = 0,
+  Failure = 1
+}
---
+++ src/Reportify/Configuration/Validation/IValidationErrorWriter.cs
@@ -0,0 +1,6 @@
+﻿namespace Reportify.Configuration.Validation;
+
+internal interface IValidationErrorWriter
+{
+  void Write(IReadOnlyList<ValidationError> validationErrors);
+}
