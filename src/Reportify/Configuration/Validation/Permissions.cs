2d7debefff841f15765c31bc1eb005a5e9aa2add
Francisco Metzger <francisco.metzger@gmail.com>
2023-10-02 20:02:37 +0200

    test(report): add tests for ConfigurationValidator

---
+++ src/Reportify/Configuration/Validation/Permissions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Reportify.Configuration.Validation;
+
+internal sealed record Permissions([property: JsonPropertyName("permissions")]
+  BrowseProjectsPermission BrowseProjectsPermission);
+
+internal sealed record BrowseProjectsPermission([property: JsonPropertyName("BROWSE_PROJECTS")]
+  Permission Permission);
+
+internal sealed record Permission([property: JsonPropertyName("havePermission")]
+  bool HavePermission);
